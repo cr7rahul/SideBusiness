ae624846d7024a1d8f8da55edf11caa34948fe00
Rahul Ramakrishnan <luhar091@gmail.com>
2021-09-17 21:35:17 +0530

    Base Project

---
+++ app/src/main/java/com/solutionsmax/movienews/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.solutionsmax.movienews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp()
+    }
+}
